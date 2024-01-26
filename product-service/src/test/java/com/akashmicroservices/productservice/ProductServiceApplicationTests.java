f9c6255ba329c858d513acb20f46b5ce75c0270d
Akash Pandey <akashp519519@gmail.com>
2024-01-26 17:45:06 +0530

    inital commit with mvn service structure

---
+++ product-service/src/test/java/com/akashmicroservices/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashmicroservices.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
