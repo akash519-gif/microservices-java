05e0d3552df1e4f656d14c4f3e395f390eb565a5
Akash Pandey <akashp519519@gmail.com>
2024-02-03 11:49:17 +0530

    microservices in one common project

---
+++ microservices-new/product-service/src/main/java/com/akashmicroservices/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akashmicroservices.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
